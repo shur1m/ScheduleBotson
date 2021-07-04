b1144add347377eff1631ad2705ae545f7290cc1
Anson Xu <85322327+shur1m@users.noreply.github.com>
2021-07-04 00:25:05 -0700

    schedule now works with dates

--- commands/misc/schedule.js
+++ commands/misc/schedule.js
@@ -1,16 +1,43 @@
+let config = require('../../config.json');
+
 let myCallback = function(message, arguments, text) {
-    let [delay, ...input] = arguments;
+    let [monthdayyear, hrsmin, ...input] = arguments;
         joinText = input.join(' ');
 
+    //parsing entered args for date
+    let [month, day, year] = monthdayyear.split("-").map(Number);
+    let [hour, minute] = hrsmin.split(":").map(Number);
+    month -= 1;
+    
+    //converting to actual dates
+    let now = Date.now();
+    let scheduledTime = new Date(year, month, day, hour, minute);
+
+    //check if date is valid
+    if ( isNaN(scheduledTime.getTime()) ){
+        message.reply(`Invalid time! \nExample command: \`${config.prefix}schedule 1-1-1975 0:00 scheduledMessage\``);
+        return;
+    }
+
+    let delay = scheduledTime.getTime() - now;
+
+    if (delay < 500){
+        message.reply("Invalid time: You cannot schedule a message in the past");
+        return;
+    }
+
+    message.reply(`Your message, *${joinText}*, has been scheduled for ${scheduledTime}`);
+
+    //scheduling message
     setTimeout(() => {
-        message.reply(`${joinText}`);            
-    }, delay * 1000);
+        message.channel.send(`${joinText}`);            
+    }, delay);
 }
 
 module.exports = {
     commands: ['schedule', 'sched'],
-    expectedArgs: '<MM-DD-YYYY> <message>',
-    minArgs: 2,
+    expectedArgs: '<MM-DD-YYYY> <HOUR:MINUTE> <message>',
+    minArgs: 3,
     maxArgs: null,
     callback: myCallback,
     permissions: [],
